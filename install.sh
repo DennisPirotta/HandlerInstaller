f8b66e36dfff07c277abedab1312c838af28aaad
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-10-25 10:26:05 +0200

    First test

---
+++ install.sh
@@ -0,0 +1,26 @@
+# Search for update and install it
+apt update && apt upgrade -y
+# Install Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# shellcheck disable=SC1090
+source ~/.bashrc
+nvm install v19.0.0
+# Install dialog extension
+apt install whiptail -y
+# Install PHP and Composer
+apt install php-cli unzip -y
+cd ~ && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+# shellcheck disable=SC2006
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+# Install git
+apt install git -y
+# Clone project
+mkdir /var/www
+cd /var/www && git clone https://github.com/DennisPirotta/handler.git
+# Setup DB
+apt install mysql-server -y
+/etc/init.d/mysql start
+mysql -uroot < setup.sql
+
---
+++ setup.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE handler;
+CREATE USER 'handler'@'%' IDENTIFIED WITH mysql_native_password BY '2U46vPx@6P!I#fT8HoH&dXqrUdqwY2TH#$Z%hISuuH%uK*BPN!';
+GRANT ALL ON handler.* TO 'handler'@'%';
