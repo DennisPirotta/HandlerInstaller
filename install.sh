4a9c34ca78c839f235b5f8c9315bea691dd71f27
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-10-25 12:21:03 +0200

    testing

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 # Search for update and install it
-#apt update
-#apt install whiptail -y
-#apt upgrade -y
+apt update && apt upgrade -y
+apt install whiptail git -y
 # Install Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-# shellcheck disable=SC1090
-source ~/.bashrc
-nvm install v19.0.0
+curl -sL https://deb.nodesource.com/setup_19.x | bash
+apt update && apt install nodejs -y
 # Install PHP and Composer
-#apt install php-cli unzip -y
-#cd ~ && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-## shellcheck disable=SC2006
-#HASH=`curl -sS https://composer.github.io/installer.sig`
-#php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-#php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+apt install php-cli php-mbstring php-xml php-bcmath php-curl unzip php-fpm -y
+/etc/init.d/php8.1-fpm start
+cd ~ && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=$(curl -sS https://composer.github.io/installer.sig)
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 ## Setup DB
-#apt install mysql-server -y
-#/etc/init.d/mysql start
-#mysql -uroot < /HandlerInstaller/setup.sql
+apt install mysql-server -y
+/etc/init.d/mysql start
+mysql -uroot < /HandlerInstaller/setup.sql
 # Clone project
-#mkdir /var/www
-#cd /var/www && git clone https://github.com/DennisPirotta/handler.git
-
+mkdir /var/www
+cd /var/www && git clone https://github.com/DennisPirotta/handler.git
+# Setup project
+cp /var/www/handler/.env.example /var/www/handler/.env
+cd /var/www/handler && npm install
+cd /var/www/handler && composer install
+cd /var/www/handler && php artisan key:generate
+cd /var/www/handler && npm run build
+# Set permissions
+chown -R www-data.www-data /var/www/handler/storage
+chown -R www-data.www-data /var/www/handler/bootstrap/cache
+# Install and configure nginx
+apt install nginx -y
+cp handler /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/handler /etc/nginx/sites-enabled/
+/etc/init.d/nginx restart
